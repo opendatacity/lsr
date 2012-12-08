4e639f4e0df21589dc55b2b146fa183cf4114bdc
Micho <github@michael-kreil.de>
2012-12-08 20:33:11 +0100

    hash checker

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,27 @@
-$(function () {
+var lastHash = '';
 
-	//alert();
+$(function () {
+	setInterval(function () {
+		var hash = window.location.hash;
+		if (hash != lastHash) {
+			$('#urlInput').val(hash.substr(1));
+			check();
+			lastHash = hash;	
+		}
+	}, 200);
+	
+	$('#checkForm').submit(function () { check() });
 });
+
+function check() {
+	var url = $('#urlInput').val();
+	
+	lastHash = '#'+url;
+	window.location.hash = lastHash;
+	
+	$('#result').html('Die Seite wird überprüft ...');
+	
+	$.getJSON('http://localhost:8080/'+url, function (data) {
+		$('#result').html(JSON.stringify(data));
+	});
+}
