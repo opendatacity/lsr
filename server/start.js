582db269afdda78c056ce772fd3829e2bda7d998
Micho <github@michael-kreil.de>
2012-12-08 21:19:19 +0100

    for debugging

--- server/start.js
+++ server/start.js
@@ -7,6 +7,8 @@ HTTP.createServer( function (req, res) {
 	var url = req.url.substr(1);
 	analyse(url, function (result) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end(JSON.stringify(result));
+		result = JSON.stringify(result);
+		console.log(result);
+		res.end(result);
 	})
 }).listen(8080);
