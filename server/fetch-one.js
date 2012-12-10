15460e491149c9c79a54240e3dac4178ff3ff506
Micho <github@michael-kreil.de>
2012-12-10 22:53:27 +0100

    fetch-one mit Beispielen

--- server/fetch-one.js
+++ server/fetch-one.js
@@ -1,9 +1,13 @@
 
 var url = process.argv[2];
 
+//url = 'http://taz.de/Prozess-um-Sachsensumpf/!107196/';
+url = 'http://www.handelsblatt.com/unternehmen/it-medien/medienbranche-springer-chef-pocht-auf-online-gema-fuer-verlage/3562678.html';
+url = 'http://www.heise.de/ct/artikel/Kulturkampf-1049862.html';
+
 require('./fetcher').analyse(
 	url,
 	function (rules) {
-		console.log(rules)
+		console.log(JSON.stringify(rules, null, '   '))
 	}
 );
