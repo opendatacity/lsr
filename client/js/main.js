4fcfab712851f0878e263c1fe5cd92b5cca6d51e
Micho <github@michael-kreil.de>
2012-12-10 22:53:08 +0100

    schönere "Metadaten"

--- client/js/main.js
+++ client/js/main.js
@@ -46,11 +46,13 @@ function check() {
 			
 			
 			html += '<h2>Im Detail</h2>';
+			
 			html += '<h3>robots.txt</h3>';
 			html += '<p><small>Die robots.txt gibt an, welche Suchmaschinen welche Inhalte anschauen dürfen</small></p>';
 			if (data.robot.length == 0) {
 				html += '<p class="alert">In der robots.txt sind für diesen Artikel keine Einschränkungen definiert. Der Artikel darf also von Google untersucht werden.</p>';
 			}
+			
 			html += '<h3>Metadaten</h3>';
 			html += '<p><small>Metadaten werden in den einzelnen Artikelseiten versteckt, um den Suchmaschinen ganz genau zu erklären, was sie dürfen und was nicht.</small></p>';
 			$.each(data.meta, function (i, meta) {
@@ -61,26 +63,26 @@ function check() {
 				
 				var robot = meta.name;
 				switch (robot.toLowerCase()) {
-					case 'robots': robot = 'alle Suchmaschinen (robots)'; break;
-					case 'googlebot': robot = 'die Google-Suche (googlebot)'; break;
-					case 'googlebot-news': robot = 'Google News (googlebot-news)'; break;
-					case 'googlebot-image': robot = 'die Google-Bildersuche (googlebot-image)'; break;
+					case 'robots': robot = '<strong>alle Suchmaschinen</strong> (sogenannte "robots")'; break;
+					case 'googlebot': robot = 'die <strong>Google-Suche</strong> (der sogenannte "googlebot")'; break;
+					case 'googlebot-news': robot = '<strong>Google-News</strong> (der sogenannte "googlebot-news")'; break;
+					case 'googlebot-image': robot = 'die <strong>Google-Bildersuche</strong> (der sogenannte "googlebot-image")'; break;
 				}
 				var rules = [];
-				$.each(meta.value, function (i, rule) {
+				$.each(meta.values, function (i, rule) {
 					switch (rule.toLowerCase()) {
-						case 'index': rule = 'index - Diese Seite soll in die Suchmaschine aufgenommen werden.'; break;
-						case 'noindex': rule = 'noindex - Diese Seite darf nicht in die Suchmaschine aufgenommen werden.'; break;
-						case 'follow': rule = 'follow - Diese Seite soll nach weiteren Artikeln durchsucht werden.'; break;
-						case 'nofollow': rule = 'nofollow - Diese Seite darf nicht nach weiteren Artikeln durchsucht werden.'; break;
-						case 'archive': rule = 'archive - Diese Seite darf archiviert werden.'; break;
-						case 'noarchive': rule = 'noarchive - Diese Seite darf nicht archiviert werden.'; break;
-						case 'snippet': rule = 'snippet - Es dürfen Textausschnitte (sogenannte Snippets) verwendet werden.'; break;
-						case 'nosnippet': rule = 'nosnippet - Es dürfen Textausschnitte (sogenannte Snippets) nicht verwendet werden.'; break;
-						case 'noodp': rule = 'noodp - Der Beschreibungstext in den Suchergebnissen darf nicht vom "open directory project" stammen.'; break;
-						case 'none': rule = 'none - Diese Seite darf weder untersucht, noch in die Suchmaschine aufgenommen werden.'; break;
+						case 'index':     text = 'Diese Seite soll in die Suchmaschine aufgenommen werden.'; break;
+						case 'noindex':   text = 'Diese Seite darf nicht in die Suchmaschine aufgenommen werden.'; break;
+						case 'follow':    text = 'Diese Seite soll nach weiteren Artikeln durchsucht werden.'; break;
+						case 'nofollow':  text = 'Diese Seite darf nicht nach weiteren Artikeln durchsucht werden.'; break;
+						case 'archive':   text = 'Diese Seite darf archiviert werden.'; break;
+						case 'noarchive': text = 'Diese Seite darf nicht archiviert werden.'; break;
+						case 'snippet':   text = 'Es dürfen Textausschnitte (sogenannte Snippets) verwendet werden.'; break;
+						case 'nosnippet': text = 'Es dürfen Textausschnitte (sogenannte Snippets) nicht verwendet werden.'; break;
+						case 'noodp':     text = 'Der Beschreibungstext in den Suchergebnissen darf nicht vom "open directory project" stammen.'; break;
+						case 'none':      text = 'Diese Seite darf weder untersucht, noch in die Suchmaschine aufgenommen werden.'; break;
 					}
-					rules.push('<li>'+rule+'</li>');
+					rules.push('<li><strong>'+rule+'</strong> - '+text+'</li>');
 				});
 				html += '<p>Diese Anweisung gilt für '+robot+':</p><ul>'+rules.join('')+'</ul>';
 			});
