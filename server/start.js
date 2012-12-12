aae37cb19650c538cb41efbac23d95263b8cc022
Micho <github@michael-kreil.de>
2012-12-12 03:27:04 +0100

    remove leading "?"

--- server/start.js
+++ server/start.js
@@ -4,6 +4,9 @@ var analyse = require('./fetcher').analyse;
 
 HTTP.createServer( function (req, res) {
 	var url = req.url.substr(1);
+	
+	while (url[0] == '?') url = url.substr(1);
+	
 	analyse(url, function (result) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 		result = JSON.stringify(result);
