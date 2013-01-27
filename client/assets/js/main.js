de81ce1ae01e27b6a1ff462092d1f2f560b8fa77
Sebastian Vollnhals <sebastian@vollnhals.info>
2013-01-27 18:42:58 +0100

    frontend rewrite / refactoring

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,3 +1,77 @@
+
+$(document).ready(function(){
+	/* hash check */
+	window.currentHash = '#';
+	var hashCheck = function(){ 
+		if (window.currentHash !== location.hash) {
+			window.currentHash = location.hash;
+			hash = location.hash.match(/^#!\/(check|get)(\/(.*))?$/);
+			if (hash) {
+				switch(hash[1]) {
+					case "get": if ($('#'+hash[3]).length > 0) $('#content').attr('class', hash[3]); break;
+					case "check": 
+						$('#content').attr('class','check');
+						if (typeof hash[3] === "undefined" || hash[3] === undefined || hash[3] == "") {
+							$('#result').attr('class','default');
+						} else {
+							$('#content').attr('class', 'check'); 
+							check(hash[3]); 
+						}
+					break;
+				}
+			}
+		}
+	}
+	if ("onhashchange" in window && (!document.documentMode || document.documentMode >= 8)) {
+		window.onhashchange = hashCheck;
+	} else {
+		setInterval(hashCheck, 50);
+	}
+	hashCheck();
+	/* button navigation */
+	$('a.btn','#result-nav').click(function(evt){
+		evt.preventDefault();
+		$btn = $(this);
+		$('a.btn.active','#result-nav').removeClass('active');
+		$(this).addClass('active');
+		$('#result').attr('class',$btn.attr('id'));
+	});
+	/* form listener */
+	$('#queryform').submit(function (e) {
+		e.preventDefault();
+		location.hash = '!/check/'+$('#url').val();
+		return false;
+	});
+});
+
+function check(url) {
+	/* check */
+	var oldClass = $('#result').attr('class');
+	$('#result').attr('class','spinner');
+	$.ajax({
+		dataType: "json",
+		url: $server+url,
+		success: function (data) {
+			$('a.btn.active','#result-nav').removeClass('active');
+			if (oldClass === 'medium'||oldClass === 'complicated') {
+				$('#result').attr('class',oldClass);
+				$('#'+oldClass).addClass('active');
+			} else {
+				$('#result').attr('class','complicated');//simple
+				$('#simple').addClass('active');
+			}
+		},
+		error: function (data) {
+			$('#result').attr('class','fail');
+			setTimeout(function(){
+				$('#result').attr('class','default');
+			},5000);
+		}
+	});
+}
+
+/*
+
 var lastHash = '';
 
 $(function () {
@@ -131,3 +205,5 @@ function check() {
 		$('#result').html('<p class="alert alert-error">Es gab einen Fehler beim Erreichen des Servers ... sorry :/</p>');
 	})
 }
+
+*/
